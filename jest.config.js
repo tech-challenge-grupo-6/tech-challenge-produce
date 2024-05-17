c1b20c69994c1f4068e63957752941a8d5854281
Renan Xavier <renansxdev@gmail.com>
2024-05-17 18:24:37 -0300

    test: add test coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,20 @@
-module.exports = { 
+module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
   roots: ['<rootDir>/tests'],
-  preset: "ts-jest", 
-  testEnvironment: "node",
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ["./src/**"],
+  collectCoverageFrom: ['./src/**'],
   coverageThreshold: {
-    "global": {
-      "lines": 90
+    global: {
+      lines: 80
     }
   },
   coveragePathIgnorePatterns: [
-    "<rootDir>/src/infra/db" // Adicionando o caminho completo da pasta que deseja ignorar
+    '<rootDir>/src/infra/db' // Adicionando o caminho completo da pasta que deseja ignorar
   ]
-};
+}
