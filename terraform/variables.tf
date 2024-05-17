fad8467998ffe627726525c3089d1c79382d53c1
Renan Xavier <renansxdev@gmail.com>
2024-05-17 09:29:15 -0300

    deploy: github actions e terraform mongodb adicionados

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "public_key" {
+  description = "The public key for the MongoDB Atlas API"
+}
+
+variable "private_key" {
+  description = "The private key for the MongoDB Atlas API"
+}
+
+variable "project_id" {
+  description = "The project ID for the MongoDB Atlas project"
+}
+
+variable "mongodb_atlas_database_username" {
+  description = "The username for the MongoDB Atlas database"
+}
+variable "mongodb_atlas_database_password" {
+  description = "The password for the MongoDB Atlas database"
+}
